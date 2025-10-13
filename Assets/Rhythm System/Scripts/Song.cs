3de38ee4c63da675df2abad0b3a05d6a2575d2d3
Heather <131394982+Heather7o55@users.noreply.github.com>
2025-10-13 11:11:31 +0100

    Very basic outline (subject to change)

---
+++ Assets/Rhythm System/Scripts/Song.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Song", menuName = "Scriptable Objects/Song")]
+public class Song : ScriptableObject
+{
+    public int bmp;
+    public int totalBeats;
+    
+}
